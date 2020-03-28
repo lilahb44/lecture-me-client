b6257d1ecd6085b5b20259aebf4085458b9acca6
Lilah <lilahb44@gmail.com>
2020-03-28 12:49:07 +0300

    Add design to welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from "react";
+import audience1 from "../assets/general/audience1.jpg";
+import audience2 from "../assets/general/audience2.jpg";
+import audience3 from "../assets/general/audience3.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Welcome({ token }) {
   const [user, setUser] = useState();
@@ -18,8 +26,55 @@ export default function Welcome({ token }) {
   if (!user) return <div>Loading...</div>;
 
   return (
-    <div className="col-lg-8 offset-lg-2">
-      <h1>Hi {user.firstName}!</h1>
-    </div>
+    <>
+      <div class="row about text-center">
+        <div class="col-12">
+          <h1>Hi {user.firstName}!</h1>
+        </div>
+      </div>
+      <Container fluid="md">
+        <Row>
+          <Col>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={audience3}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>“Easy web site to use” </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={audience2}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>“It was a very wonderful experience.”</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={audience1}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>“your service are excellent”</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
