226f9601626582b55c9336a2d9e2c61efccaaf18
Lilah <lilahb44@gmail.com>
2020-05-26 21:48:41 +0300

    Add paypal button to orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { PayPalButton } from "react-paypal-button-v2";
 import MaterialTable from "material-table";
 import styled from "styled-components";
 
@@ -8,8 +9,21 @@ const Wrapper = styled.div`
   width: 96%;
 `;
 
+const useLoadPaypal = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://www.paypal.com/sdk/js?client-id=ASZTQImLWA4NK4i1Zqrgy_iGyvifIQFkB6rKFOgeR8c4MJmutb9OW5PX1FGgNUu2N49ehyFCNYOe38uH&currency=ILS&disable-funding=credit,card`;
+    script.addEventListener("load", () => setLoaded(true));
+    document.body.appendChild(script);
+  }, []);
+
+  return loaded;
+};
+
 export default function Orders({ token }) {
   const [order, setOrder] = useState();
+  const paypalLoaded = useLoadPaypal();
 
   const refreshData = () =>
     fetch(`https://lecture-me.herokuapp.com/userApi/orders`, {
@@ -31,7 +45,7 @@ export default function Orders({ token }) {
     <Wrapper>
       <div class="row about text-center">
         <div class="col-12">
-          <h1>Your Orders</h1>
+          <h1>Orders</h1>
           <br></br>
         </div>
         <div class="col-12"></div>
@@ -40,13 +54,52 @@ export default function Orders({ token }) {
       <br></br>
 
       <MaterialTable
-        title="Your orders"
+        title="Orders"
         columns={[
           { title: "Invitation date", field: "date" },
           { title: "Group", field: "groupName" },
           { title: "Lecturer", field: "lecturer" },
           { title: "Address", field: "address" },
           { title: "Status of lecturer", field: "status" },
+          {
+            title: "Payment",
+            render: (rowData) =>
+              paypalLoaded &&
+              rowData.status === "Accept" && (
+                <PayPalButton
+                  createOrder={(data, actions) => {
+                    return actions.order.create({
+                      purchase_units: [
+                        {
+                          amount: {
+                            currency_code: "ILS",
+                            value: "3000",
+                          },
+                          custom_id: "123",
+                        },
+                      ],
+                      application_context: {
+                        shipping_preference: "NO_SHIPPING", // default is "GET_FROM_FILE"
+                      },
+                    });
+                  }}
+                  onSuccess={(details, data) => {
+                    alert(
+                      "Transaction completed by " +
+                        details.payer.name.given_name
+                    );
+
+                    // OPTIONAL: Call your server to save the transaction
+                    return fetch("/paypal-transaction-complete", {
+                      method: "post",
+                      body: JSON.stringify({
+                        orderId: data.orderID,
+                      }),
+                    });
+                  }}
+                />
+              ),
+          },
         ]}
         data={order}
       />
